7e60ec8ebcf87d55e85f1bede54210ab83bc0506
Stefan Busnita <stefanbusnita@yahoo.com>
2018-01-16 21:01:00 +0100

    Small renaming

--- CamelCase/main.go
+++ CamelCase/main.go
@@ -13,17 +13,17 @@ func main() {
 	fmt.Scanf("%s\n", &input)
 	fmt.Println("The input is: ", input)
 
-	allArray := []byte(input)
+	inputStringSlice := []byte(input)
 
 	buffer := make([]byte, 0)
 
-	for index, ch := range allArray {
+	for index, ch := range inputStringSlice {
 		character := string(ch)
-		if strings.ToUpper(character) == character || index == len(allArray)-1 {
-			if index == len(allArray)-1 {
+		if strings.ToUpper(character) == character || index == len(inputStringSlice)-1 {
+			if index == len(inputStringSlice)-1 {
 				buffer = append(buffer, ch)
 			}
-			fmt.Println("A word is", string(buffer))
+			fmt.Println(index, string(buffer))
 			buffer = nil
 		}
 		buffer = append(buffer, ch)
